9e5ed2e7ba5356b9501cd21b12fe0424576ff40d
An-Mao <lxxl@live.cn>
2026-08-30 17:22:10 +0800

    26083000

--- src/main/java/dev/anye/mc/cores/helper/client/ClientEntityHelper.java
+++ src/main/java/dev/anye/mc/cores/helper/client/ClientEntityHelper.java
@@ -2,13 +2,18 @@ package dev.anye.mc.cores.helper.client;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.LivingEntity;
-import net.neoforged.neoforge.client.ClientHooks;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 
 public class ClientEntityHelper {
 	public static boolean checkView(Minecraft minecraft, LivingEntity entity) {
 		if (minecraft.player != null) {
-			return ClientHooks.isNameplateInRenderDistance(entity, minecraft.getEntityRenderDispatcher().distanceToSqr(entity)) && minecraft.player.hasLineOfSight(entity) && entity == minecraft.getEntityRenderDispatcher().crosshairPickEntity;
+			return isNameplateInRenderDistance(entity, minecraft.getEntityRenderDispatcher().distanceToSqr(entity)) && minecraft.player.hasLineOfSight(entity) && entity == minecraft.getEntityRenderDispatcher().crosshairPickEntity;
 		}
 		return false;
+	}
+
+	public static boolean isNameplateInRenderDistance(LivingEntity entity, double squareDistance) {
+		double value = entity.getAttributeValue(Attributes.NAME_TAG_DISTANCE);
+		return squareDistance <= value * value;
 	}
 }
